5d105b04dd5484aea409e3ad1708951236a8b087
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2025-02-06 01:09:47 +0530

    Removed global object reference

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,23 +6,11 @@ if (!MONGODB_URI) {
     throw new Error('Please define MONGODB_URI environment variable');
 }
 
-// Define the type for the cache
-interface MongooseCache {
+// Module-scoped cache
+let cached: {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
-}
-
-// Extend the global object to include the `mongoose` property
-declare global {
-    var mongoose: MongooseCache;
-}
-
-// Initialize `cached` with the correct type
-let cached: MongooseCache = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
+} = { conn: null, promise: null };
 
 async function dbConnect(): Promise<typeof mongoose> {
     if (cached.conn) {
